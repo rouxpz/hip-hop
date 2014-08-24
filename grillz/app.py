8a54b16ccba4eda404221ccf3386d288183e9780
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2014-08-24 19:35:59 -0400

    pulling in words from dictionaries, organized python code into functions

--- grillz/app.py
+++ grillz/app.py
@@ -5,49 +5,79 @@ import os
 artist = raw_input("Please enter artist: ")
 title = raw_input("Please enter song title: ")
 
-filename = "LYRIC-FILES/" + artist + "-" + title + ".txt"
-
-file = open(filename, "w")
-
 song = []
+keys = []
 
-lyrics = songs.searchSong(artist, title, "lyrics")
+def keyPhrases(category):
 
-for lyric in lyrics:
-	r = re.search(r'\[(.+?)\]', lyric)
-	# if r == None:
+	file = open('dictionaries/' + category + '.txt', 'r')
+	phrases = file.read()
+	phrases = phrases.split('\n')
+	keys.append(phrases)
+	file.close()
 
-	if r != None:
-		# print lyric
-		delete = r.group(0)
-		lyric = lyric.replace(delete, '')
+# print poverty
+# print wealth
 
-		rnew = re.search(r'\[(.+?)\]', lyric)
+def getLyrics():
 
-		if rnew != None:
-			deletenew = rnew.group(0)
-			lyric = lyric.replace(deletenew, '')
+	lyrics = songs.searchSong(artist, title, "lyrics")
+
+	for lyric in lyrics:
+		r = re.search(r'\[(.+?)\]', lyric)
+		# if r == None:
+
+		if r != None:
+			# print lyric
+			delete = r.group(0)
+			lyric = lyric.replace(delete, '')
+
+			rnew = re.search(r'\[(.+?)\]', lyric)
+
+			if rnew != None:
+				deletenew = rnew.group(0)
+				lyric = lyric.replace(deletenew, '')
+
+			else:
+				pass
 
 		else:
 			pass
 
-	else:
-		pass
+		lyric = lyric.replace('\n', ' ').replace('(', '').replace(')', '').replace(',', '').replace('-', ' ')
+		lyric = lyric.strip()
+		
+		words = lyric.split(' ')
 
-	lyric = lyric.replace('\n', ' ').replace('(', '').replace(')', '').replace(',', '').replace('-', ' ')
-	lyric = lyric.strip()
-	
-	words = lyric.split(' ')
+		for word in words:
+			song.append(word)
 
-	for word in words:
-		song.append(word)
-	
+def analyzeSong():
+	filename = "LYRIC-FILES/" + artist + "-" + title + ".txt"
+
+	file = open(filename, "w")
+
+	for word in song:
+		file.write(word)
+		file.write(" ")
+
+	file.close()
+
+	print "File is saved at " + filename
+
+
+
+keyPhrases("poverty")
+keyPhrases("wealth")
+
+poverty = keys[0]
+wealth = keys[1]
+
+# print poverty
+# print wealth
+
+getLyrics()
+analyzeSong()
+
 # print song
 
-for word in song:
-	file.write(word)
-	file.write(" ")
-
-file.close()
-
-print "File is saved at " + filename
